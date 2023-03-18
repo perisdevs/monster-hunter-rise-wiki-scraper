29dd511dbd8b8602fdd8fd1ee8364b8f8b972d8d
Peris <perisdevs@gmail.com>
2023-03-17 22:33:13 -0700

    getTableAsArray function

--- src/util/util.ts
+++ src/util/util.ts
@@ -2,8 +2,19 @@ import { readFileSync } from 'fs';
 import * as jsdom from 'jsdom';
 const { JSDOM } = jsdom;
 
-export function getFileAsDOM(filepath: string) {
+export function getFileAsDOM(filepath: string): Document {
     const fileString = readFileSync(filepath, 'utf-8');
     const { document } = (new JSDOM(fileString)).window;
     return document;
+}
+
+export function getTableAsArray<T extends Object>(
+    table: HTMLTableSectionElement,
+    rowParser: (row: HTMLTableRowElement) => T) : T[] {
+        const ObjectArray: T[] = [];
+        const rows = Array.from(table.getElementsByTagName('tr'));
+        rows.forEach((row) => {
+            ObjectArray.push(rowParser(row));
+        });
+        return ObjectArray;
 }
