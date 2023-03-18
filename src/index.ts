aaa58d9541edc1294f282b3e2beebdda92d61df4
Peris <perisdevs@gmail.com>
2023-03-17 21:14:21 -0700

    Added TS stub

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/*import { Requests } from './consts.js';
+import { Scraper } from './scrapers.js';
+
+const scraper = new Scraper();
+scraper.addRequests(Requests);
+scraper.scrapeAll();*/
+console.log('TS is awesome');
+
+
---
+++ src/scrapers/scrapers.ts
@@ -0,0 +1,4 @@
+/*export class Scrape {
+    filepath: string;
+    scrapingFunction: ()
+}*/
